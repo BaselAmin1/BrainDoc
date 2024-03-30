ecfd63d754998a0672ddb96cb405565805e4d82e
Basel Amin <basel.amin77@gmail.com>
2024-03-30 17:21:43 +0200

    launch location

--- lib/features/doctor_details/ui/widgets/doctor_location_widget.dart
+++ lib/features/doctor_details/ui/widgets/doctor_location_widget.dart
@@ -4,6 +4,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DoctorLocationWidget extends StatelessWidget {
   DoctorLocationWidget({
@@ -16,54 +17,70 @@ class DoctorLocationWidget extends StatelessWidget {
   final DoctorDetailsScreen widget;
   var mapController;
   Set<Marker> marker;
+  void _openMap() async {
+    // Replace lat and long with your desired latitude and longitude
+    String latitude = widget.doctor['location'].latitude.toString();
+    String longitude = widget.doctor['location'].longitude.toString();
+
+    // The URL format to open the map with specific latitude and longitude
+    String url =
+        'https://www.google.com/maps/search/?api=1&query=$latitude,$longitude';
+
+    await canLaunch(url) ? await launch(url) : throw 'Could not launch $url';
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 8.h),
-        child: Row(
-          children: [
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'workLocation'.tr(),
-                    style: TextStyles.textStyle18.copyWith(),
-                  ),
-                  SizedBox(height: 4.h),
-                  Text(
-                    widget.doctor['address'],
-                    style: TextStyles.textStyle14,
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 8.h),
-                    clipBehavior: Clip.antiAlias,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(18.r),
-                      color: const Color.fromARGB(255, 193, 208, 255),
+        child: GestureDetector(
+          onTap: () {
+            _openMap();
+          },
+          child: Row(
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'workLocation'.tr(),
+                      style: TextStyles.textStyle18.copyWith(),
                     ),
-                    width: double.infinity,
-                    height: 150.h,
-                    child: GoogleMap(
-                      initialCameraPosition: CameraPosition(
-                        target: LatLng(widget.doctor['location'].latitude,
-                            widget.doctor['location'].longitude),
-                        zoom: 15,
+                    SizedBox(height: 4.h),
+                    Text(
+                      widget.doctor['address'],
+                      style: TextStyles.textStyle14,
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 8.h),
+                      clipBehavior: Clip.antiAlias,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(18.r),
+                        color: const Color.fromARGB(255, 193, 208, 255),
                       ),
-                      myLocationButtonEnabled: false,
-                      onMapCreated: (GoogleMapController controller) {
-                        mapController = controller;
-                      },
-                      markers: marker,
+                      width: double.infinity,
+                      height: 150.h,
+                      child: GoogleMap(
+                        initialCameraPosition: CameraPosition(
+                          target: LatLng(widget.doctor['location'].latitude,
+                              widget.doctor['location'].longitude),
+                          zoom: 15,
+                        ),
+                        myLocationButtonEnabled: false,
+                        onMapCreated: (GoogleMapController controller) {
+                          mapController = controller;
+                        },
+                        markers: marker,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
